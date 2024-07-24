9cb4bf8cfed57c097cbe5e27d5684a738ae0188e
khoaipx <phamxuankhoai@gmail.com>
2024-07-24 17:31:17 +0700

    New theme

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import Footer from './Footer';
+import Hero from './Hero';
+import Highlights from './Highlights';
+import NavBar from './NavBar';
+import PageLayout from './PageLayout';
+import Experience from './Experience';
+import Education from "./Education";
+import Award from "./Award";
+import Publication from "./Publication";
+
+export { Footer, NavBar, PageLayout, Hero, Highlights, Experience, Education, Award, Publication };
---
+++ data/blogs.js
@@ -0,0 +1,10 @@
+export const BlogList = [
+    {
+        id: 1,
+        title: 'Câu chuyện của tôi',
+        description: 'Đôi điều giới thiệu ngắn gọn về bản thân tôi',
+        tag: 'Personal',
+        date: 'May 22, 2023',
+        slug: '/posts/my-story'
+    },
+];
